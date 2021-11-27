4488a78d54eab15fb3de1428a9be6dbff5acc687
unloccode <unloccode@gmail.com>
2021-11-27 22:18:05 +0300

    change log 3.6

--- config/env.js
+++ config/env.js
@@ -1,25 +1,9 @@
 //offline
-module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '123n',
-    DB: 'mytodos',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
-
-//online
-//freemysqlhosting
 //module.exports = {
-//    HOST: 'sql11.freemysqlhosting.net',
-//    USER: 'sql11454217',
-//    PASSWORD: 'CRkAgqcZmJ',
-//    DB: 'sql11454217',
+//    HOST: 'localhost',
+//    USER: 'root',
+//    PASSWORD: '123n',
+//    DB: 'mytodos',
 //    dialect: 'mysql',
 //    pool: {
 //        max: 5,
@@ -28,3 +12,19 @@ module.exports = {
 //        idle: 10000
 //    }
 //};
+
+//online
+freemysqlhosting
+module.exports = {
+    HOST: 'sql11.freemysqlhosting.net',
+    USER: 'sql11454217',
+    PASSWORD: 'CRkAgqcZmJ',
+    DB: 'sql11454217',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
